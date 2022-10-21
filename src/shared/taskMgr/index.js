492cc03bafd0b3532ba1d5f917c077d2d85206b2
liaohui5 <liaohui5@foxmail.com>
2022-10-21 15:58:12 +0800

    移除了taskMgr单例模式,直接导出一个对象

--- src/shared/taskMgr/index.js
+++ src/shared/taskMgr/index.js
@@ -2,37 +2,27 @@
 import TaskQueue from "./TaskQueue";
 import AsyncTaskQueue from "./AsyncTaskQueue";
 
-class TaskMgr {
-  constructor() {
-    this.instance = null;
-  }
-
-  // 创建同步任务队列
-  createTaskQueue(tasks) {
-    return new TaskQueue(tasks);
-  }
-
-  // 创建异步任务队列
-  createAsyncTaskQueue(asyncTask, tasks) {
-    return new AsyncTaskQueue(asyncTask, tasks);
-  }
-
-  // 通知队列执行对应 type 的任务
-  notify(type, taskPool) {
-    return function (state, payload) {
-      for (let [key, val] of Object.entries(taskPool)) {
-        type === key && val.execute(state, payload);
-      }
-    };
-  }
-
-  // 单例
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = new TaskMgr();
-    }
-    return this.instance;
-  }
+// 创建同步任务队列
+function createTaskQueue(tasks) {
+  return new TaskQueue(tasks);
 }
 
-export default TaskMgr.getInstance();
+// 创建异步任务队列
+function createAsyncTaskQueue(asyncTask, tasks) {
+  return new AsyncTaskQueue(asyncTask, tasks);
+}
+
+// 通知队列执行对应 type 的任务
+function notify(type, taskPool) {
+  return function (state, payload) {
+    for (let [key, val] of Object.entries(taskPool)) {
+      type === key && val.execute(state, payload);
+    }
+  };
+}
+
+export default {
+  createTaskQueue,
+  createAsyncTaskQueue,
+  notify,
+};
